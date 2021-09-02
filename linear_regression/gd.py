c619cf44f201bc152faa116179db9a3ee3ac90be
carryzheng8023 <carryzheng8023@gmail.com>
2021-09-02 04:37:58 +0000

    reformat

--- linear_regression/gd.py
+++ linear_regression/gd.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 # y = wx + b
 def compute_error_for_line_given_points(b, w, points):
     totalError = 0
@@ -9,6 +10,7 @@ def compute_error_for_line_given_points(b, w, points):
         totalError += (y - (w * x + b)) ** 2
     return totalError / float(len(points))
 
+
 def step_gradient(b_current, w_current, points, learningRate):
     b_gradient = 0
     w_gradient = 0
@@ -16,11 +18,12 @@ def step_gradient(b_current, w_current, points, learningRate):
     for i in range(0, len(points)):
         x = points[i, 0]
         y = points[i, 1]
-        b_gradient += -(2/N) * (y - ((w_current * x) + b_current))
-        w_gradient += -(2/N) * x * (y - ((w_current * x) + b_current))
+        b_gradient += -(2 / N) * (y - ((w_current * x) + b_current))
+        w_gradient += -(2 / N) * x * (y - ((w_current * x) + b_current))
     new_b = b_current - (learningRate * b_gradient)
     new_m = w_current - (learningRate * w_gradient)
     return [new_b, new_m]
+
 
 def gradient_descent_runner(points, starting_b, starting_w, learning_rate, num_iterations):
     b = starting_b
@@ -29,11 +32,12 @@ def gradient_descent_runner(points, starting_b, starting_w, learning_rate, num_i
         b, w = step_gradient(b, w, np.array(points), learning_rate)
     return [b, w]
 
+
 def run():
     points = np.genfromtxt("data.csv", delimiter=",")
     learning_rate = 0.0001
-    initial_b = 0 # initial y-intercept guess
-    initial_w = 0 # initial slope guess
+    initial_b = 0  # initial y-intercept guess
+    initial_w = 0  # initial slope guess
     num_iterations = 1000
     print("Starting gradient descent at b = {0}, w = {1}, error = {2}"
           .format(initial_b, initial_w,
@@ -45,6 +49,7 @@ def run():
           format(num_iterations, b, w,
                  compute_error_for_line_given_points(b, w, points))
           )
+
 
 if __name__ == '__main__':
     run()
